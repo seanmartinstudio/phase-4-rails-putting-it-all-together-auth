24ec036e555d0b9b815508df6eed88603a080ebf
Sean Martin <sean@seanmartinstudio.com>
2022-09-09 12:35:40 -0700

    Sign Up and Auto LogIn complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :recipes
   resources :users
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
 end
