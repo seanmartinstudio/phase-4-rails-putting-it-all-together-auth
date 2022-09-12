ac5d35134661e1d37b154e96313481ac65d675f7
Sean Martin <sean@seanmartinstudio.com>
2022-09-12 15:34:53 -0700

    Working on last feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,28 @@
 class RecipesController < ApplicationController
+
+    def index 
+        user = User.find_by(username: params[:username])
+        if session[:user_id]
+            render json: Recipe.all, status: :created
+        else  
+            render json: { errors: ["Not Authorized"] }, status: :unauthorized
+        end
+    end
+
+    def create 
+        user = User.find_by(username: params[:username])
+        if session[:user_id]
+            recipe = user.recipes.create!(recipe_params)
+            redner json: recipe, status: :created
+        elsif
+            
+        end
+    end
+
+    private
+
+    def recipe_params 
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+
 end
--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,5 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :instructions, :minutes_to_complete
+
+  has_one :user
 end
